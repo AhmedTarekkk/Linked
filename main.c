6b92f2c80da0a694858e8f351709755284069c66
Ahmed Tarek <ahmed.tarek55525@gmail.com>
2021-07-10 22:47:56 +0200

    Student1 Work

---
+++ main.c
@@ -0,0 +1,28 @@
+#include "stdafx.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+
+//----------- start student 1
+
+struct student_t{
+char name[20]; // assume that the longest student name consist of 20 char
+int id;
+int birth[3]; // day month year
+int score;
+};
+
+//------------- end student 1
+int main()
+{
+	int n;
+	char input [1];
+	printf("Welcome To DataStructure Testing\n");
+	getchar();
+
+return 0;}
+
+
+
+
